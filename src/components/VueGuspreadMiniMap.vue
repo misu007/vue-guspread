c7170daf593b1217fb06406063fe36c9ad182dd9
Michikazu Sugahara <misu007.github@gmail.com>
2020-03-08 14:29:17 +0900

    FIX - minimap background repeat

--- src/components/VueGuspreadMiniMap.vue
+++ src/components/VueGuspreadMiniMap.vue
@@ -157,6 +157,7 @@ export default {
     background-color: #bbb;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='20' viewBox='0 0 100 20'%3E%3Cpath fill='white' d='M0,0 L99,0 99,19 0,19'%3E%3C/path%3E%3C/svg%3E");
     background-size: 25px 5px;
+    background-repeat: repeat;
     pointer-events: all;
     box-shadow: 0 0 15px -5px #aaa;
   }
